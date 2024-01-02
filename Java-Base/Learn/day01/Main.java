6d80496071fe2e7a50ca93c611932d62a5eb8e3e
HeZaoCha <1224657703@qq.com>
2024-01-03 00:59:33 +0800

    On branch HTML-Base Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   README.md

--- Java-Base/Learn/day01/Main.java
+++ Java-Base/Learn/day01/Main.java
@@ -11,11 +11,25 @@ public class Main {
         }
         Summation summation = new Summation();
         Division division = new Division();
+        NineNineTable m_table = new NineNineTable();
         System.out.println("----------------Part 1----------------");
         System.out.println("1. 1+2+3+...+100的和为：" + summation.sum(numbers));
         System.out.println("2. 1+2+3+...+100的偶数和为：" + summation.sum(numbers, 1, 2));
         System.out.println("3. 1+2+3+...+100的奇数和为：" + summation.sum(numbers, 0, 2));
         System.out.println("4. 1+2+3+...+100中的既能被3整除又能被6整除的数字之和为：" + summation.sum((division.div(division.div(numbers, 3), 6))));
-        System.out.println("5. 九九乘法表：\n");
+        System.out.println("\n5. 九九乘法表：");
+        m_table.nine_nine_table();
+    }
+}
+
+
+class NineNineTable {
+    public void nine_nine_table() {
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + "*" + i + "=" + (i * j) + "\t");
+            }
+            System.out.println();
+        }
     }
 }
