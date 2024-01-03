bdcd45f126aaf8d469bdc49384d78ee064747559
HeZaoCha <1224657703@qq.com>
2024-01-03 12:34:05 +0800

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   README.md

--- Java-Base/Learn/day01/MathCalculations/Summation.java
+++ Java-Base/Learn/day01/MathCalculations/Summation.java
@@ -1,25 +1,22 @@
 package Learn.day01.MathCalculations;
 
-import com.sun.org.apache.xpath.internal.operations.String;
-
 import java.util.List;
 
 public class Summation {
-    public <T> T sum(List<T> numbers) {
+    public <T extends Number> T sum(List<T> numbers) {
         return sum(numbers, 0, 1);
     }
 
-    public <T> T sum(List<T> numbers, Integer start, Integer step) {
+    public <T extends Number> T sum(List<T> numbers, Integer start, Integer step) {
         T sum_temp = numbers.get(start);
-        for (T value : numbers.subList(start + step, numbers.size())) {
-            sum_temp = sum(value, sum_temp);
+        for (int i = start + step; i < numbers.size(); i += step) {
+            T value = numbers.get(i);
+            sum_temp = (T) sum((value), sum_temp);
         }
         return sum_temp;
     }
 
-    private <T extends Number> T sum(T num1, T num2) {
-        return (String) (num1.doubleValue() + num2.doubleValue());
+    private <T extends Number> Double sum(T num1, T num2) {
+        return num1.doubleValue() + num2.doubleValue();
     }
-
-
 }
